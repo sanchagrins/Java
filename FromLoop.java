838c4ad7e2fa863f340aa0ed4bdd2d7c89bada41
Stephen <sanchagrins@gmail.com>
2016-11-10 15:50:29 +0900

    Format output/code cleanup/comments

--- FromLoop.java
+++ FromLoop.java
@@ -3,26 +3,27 @@
  * Date:	11/09/2016
  * Purpose:	Adaptation of Coursera exercise for Python.
  * 		Reads through a file and finds all lines that
- *              Start with "From :" and extracts Email addresses.
+ *              start with "From :" and extracts/stores
+ *              unique email addresses using Regular Expressions.
  */
 
 import java.io.*;  // Imports BufferReader and FileReader classes
 import java.util.regex.*;  // Imports Pattern and Matcher classes
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.*;  // Imports for ArraryList and HashSet
 
 public class FromLoop {
     public static void main(String[] args) throws java.io.IOException {
 
       //  Declare Local Variables
       int count = 0;
+      int emailNum;
       String line;
       String pattern = "\\S+@\\S+";
 
       // Create a Pattern object
       Pattern r = Pattern.compile(pattern);
 
-      // Create List Container 
+      // Create List Container to store emails 
       ArrayList<String> l = new ArrayList<String>();
       
       //  Read in the file
@@ -34,18 +35,26 @@ public class FromLoop {
  
             //  Extracts lines that start with "From: "
             if (line.startsWith("From: ")) {
-               Matcher m = r.matcher(line);
+               // Creates Regular Expression Matcher
+               Matcher m = r.matcher(line); 
+
+               // Searches line for Regular Expression
                if (m.find()) {
-                  System.out.println("Found Email: " + m.group(0));
                   l.add(m.group(0));  //  Adds email to list
                }  // End if
-               count++;
             }  // End if
+            count++;
       }  // End While
-      System.out.println("Number of Lines: " + count);
-      System.out.println(l);
+      
+      // Removes duplicates from list by creating a new HashSet
       ArrayList<String> newList = new ArrayList<String>(new HashSet<String>(l));
-      System.out.println(newList);
+
+      // Reports number of emails found
+      emailNum = newList.size();
+      System.out.println("-----Unique Emails-----");
+      newList.forEach(System.out::println);
+      System.out.println("-----------------------");
+      System.out.println(emailNum + " unique emails found in " + count + " lines.");
     }  // End Main
 }  //  End FromLoop
 
