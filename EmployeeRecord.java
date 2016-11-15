a00f303f7a550ebeb37419407b4e1e218a91d194
Stephen <sanchagrins@gmail.com>
2016-11-15 20:27:45 +0900

    Comments

--- EmployeeRecord.java
+++ EmployeeRecord.java
@@ -1,26 +1,39 @@
+/* File:	EmployeeRecord.java
+ * Author:	Stephen
+ * Date:	11/15/2016
+ * Purpose:	Provide code for a simple class using
+ *		at least one constructor, two methods 
+ *		and two fields.
+ */
+
 import java.util.*;
 
 class EmployeeRecord {
     public static void main(String[] args) {
+
+       // Local Varaibles
        String firstName;
        String lastName;
        String id;
        String email;
        int num;
 
+       // Read in number of employees to add
        Scanner in = new Scanner(System.in);
        System.out.print("Enter number of employees to add: ");
        num = Integer.parseInt(in.nextLine());
 
+       // Create new employee array
        Employee[] employee = new Employee[num];
-       //Employee employee1 = new Employee();
-       //System.out.println(employee.length);
        
+       // Loops through employee array to gather input for Employee objects
        for (int i=0; i<employee.length;i++) {
+
+          // Creates new Employee Object with default constructor
           employee[i] = new Employee();
           
+          // Assigns user input to variables 
           System.out.println("--------Input Employee #" + (i+1) + " Data--------");
-
           System.out.print("First Name: ");
           firstName = in.nextLine();
 
@@ -30,6 +43,7 @@ class EmployeeRecord {
           System.out.print("Email: ");
           email = in.nextLine();
           
+          // Sends variables to setter methods
           employee[i].setID(3802);
           employee[i].setFirstName(firstName);
           employee[i].setLastName(lastName);
@@ -37,7 +51,7 @@ class EmployeeRecord {
 
        }
        
-//       System.out.println(employee[1].showData());
+       // Loops through employee array generate report
        for (int i=0; i<employee.length;i++) {
           System.out.println(employee[i].showData());
        }
