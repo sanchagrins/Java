075f8427a89d03445f985c3d620470c79b12009f
Stephen <sanchagrins@gmail.com>
2016-11-22 14:51:28 +0900

    Code restructuring; Added getAvgWspd()

--- Bouy.java
+++ Bouy.java
@@ -8,8 +8,6 @@ public class Bouy {
 	private String dateRange[] = {"25", "26", "27", "28", "29", "30"};
 	private boolean foundMonth;
 	private boolean foundDate;
-	private int lineNum=0;
-	private int matchNum=0;
 	private String data;
 
 	public Bouy() {
@@ -34,10 +32,12 @@ public class Bouy {
 		return id;
 	}
 
-	public StringBuilder getData() throws Exception {
+	public String[] getData() throws Exception {
 		String header;
 		StringBuilder results = new StringBuilder();
 		String line;
+		Integer lineNum = new Integer(1);
+		Integer matchNum = new Integer(1);
 
 		URL addr = new URL(url);
 		HttpURLConnection conn = (HttpURLConnection) addr.openConnection();
@@ -48,20 +48,45 @@ public class Bouy {
 		results.append(header);
 		
 		while ((line = bouyData.readLine()) != null) {
-			String readData[] = line.split(" ");
+			String readData[] = line.split("\\s+");
 			String date = readData[2];
 			boolean monthFound = readData[1].equals("08");
 			boolean dateFound = Arrays.asList(dateRange).contains(date);
 			if ((monthFound) && (dateFound)) {
 				results.append(line + "\n");
+				matchNum++;
 			}
+			lineNum++;
 		}
-		return results;
+		String[] data = {results.toString(), lineNum.toString(), matchNum.toString()};
+		return data;
 
 	}
-	
 
+	public HashMap getAvgWspd(String[] bouyData) throws Exception {
+		String rawData = bouyData[0];
+		HashMap<String, Double> avgWspd = new HashMap<String, Double>();
 
-		
-
+		for (int i=0;i<dateRange.length;i++) {
+			double meanWspd=0.0;
+			double sumWspd=0.0;
+			double cnt=0.0;
+			System.out.println(dateRange[i]);
+			String line = null;
+			BufferedReader br = new BufferedReader(new StringReader(rawData));
+			while ((line=br.readLine()) != null) {
+				String readData[] = line.split("\\s+");
+				boolean dateCheck = readData[2].equals(dateRange[i]);
+				if (dateCheck) {
+					System.out.println(readData[6]);
+					double wspd = Double.parseDouble(readData[6]);
+					sumWspd = sumWspd + wspd;
+					cnt++;
+					meanWspd = sumWspd/cnt;
+				}
+				avgWspd.put(dateRange[i], meanWspd);
+			}
+		}
+		return avgWspd;
+	}
 }
