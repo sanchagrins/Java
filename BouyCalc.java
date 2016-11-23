cbc6bdabfc6338a81be880136b43a41c7847fc9e
Stephen <sanchagrins@gmail.com>
2016-11-23 14:43:45 +0900

    Comments

--- BouyCalc.java
+++ BouyCalc.java
@@ -1,18 +1,31 @@
+/* File:	BouyCalc.java
+ * Author:	sanchagrins
+ * Date:	11-23-2016
+ * Purpose:	Connects to nbdc.noaa.gove and obtains bouy data.
+ * 			Basic calculations are made for average wind speed,
+ * 			wind gust, wave height and barometric pressure. The
+ * 			result are formated and a report is generated.
+ */
+
 import java.io.*;
 import java.util.*;
 import java.net.*;
 
 public class BouyCalc {
 	public static void main(String[] args) throws Exception {
+		// Local Variables
 		String lines;
 		String matches;
 		HashMap avgWspd;
 		HashMap avgGust;
 		HashMap avgWvht;
 		HashMap avgPres;
-
+		
+		// Creates a new bouy object with the default constructor
 		Bouy bouy = new Bouy();
-		System.out.println("Fetching data from nbc.noaa.gov:");
+
+		// Fetches the bouy data fom ndbc.noaa.gov
+		System.out.println("Fetching data from ndbc.noaa.gov:");
 		System.out.println("Date Range: 08-25-2005 - 08-31-2005");
 		System.out.println("NDBC ID: " + bouy.getID());
 		String[] results = bouy.getData();
@@ -38,15 +51,17 @@ public class BouyCalc {
 		avgGust = bouy.getAvgGust(results);
 		System.out.println("Done.");
 
-		
+		// Calculate Average Wave Height
 		System.out.print("Calculating avg. wave height for date range...");
 		avgWvht = bouy.getAvgWvht(results);
 		System.out.println("Done.");
 
+		// Calculate Average Barometric Pressure
 		System.out.print("Calculating avg barometric pressure for date range...");
 		avgPres = bouy.getAvgPres(results);
 		System.out.println("Done.\n");
 
+		// Generate and display the Report
 		String report = bouy.generateReport(avgWspd, avgGust, avgWvht, avgPres);
 		System.out.println(report);
 	}
